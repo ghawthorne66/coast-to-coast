8d04e8e2e4c7f9c9af2eda9cb1334b2243d0922e
Muhammad Ali <alipakistani90@gmail.com>
2020-05-27 08:25:36 +0500

    Menu Structure Fixed with Responsiveness

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,7 +1,9 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
+import {
+  Row, Col, Container, Navbar, Nav, NavDropdown,
+} from 'react-bootstrap'
 import styled from 'styled-components'
 import Image from '../image'
 import Menu from './menu'
@@ -15,20 +17,40 @@ const Header = () => (
   <header id="site-header">
     <Container className="my-3">
       <Row>
-        <Col lg="4" className="">
-          <LogoCont className="logo-cont">
-            <Link to="/">
-              <Image imgName="logo.png" />
-            </Link>
-          </LogoCont>
+        <Col>
+          <Navbar bg="light" expand="md">
+            <Navbar.Brand href="#home">
+              <LogoCont className="logo-cont">
+                <Link to="/">
+                  <Image imgName="logo.png" />
+                </Link>
+              </LogoCont>
 
+            </Navbar.Brand>
+
+            <div className="site-nav-cont w-100 d-md-flex flex-md-column align-items-md-end text-center">
+              <div className="cta">
+                <a href="tel:3213513869">(321) 351-3869</a>
+              </div>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="justify-content-end">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+
+            </div>
+          </Navbar>
         </Col>
-        <Col lg="8">
-          <div className="py-3 d-flex flex-column align-items-center align-items-lg-end">
-            <a href="tel:3213513869">(321) 351-3869</a>
-            <Menu />
-          </div>
-        </Col>
+
       </Row>
     </Container>
   </header>
