bc3cd553dbb807806a0f59f63a0c4b64bdf6f1d9
Muhammad Ali <alipakistani90@gmail.com>
2020-05-27 07:32:49 +0500

    Cleanup

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 import styled from 'styled-components'
-import Image from '../common/image'
+import Image from './image'
 
 const LogoCont = styled.div`
   width: 300px;
@@ -19,14 +19,14 @@ const CtaCont = styled.div`
 
 `
 
-const Header = ({ siteTitle }) => (
-  <header id="acumenlegal-header">
+const Header = () => (
+  <header id="site-header">
     <Container className="my-5 py-3 px-5">
       <Row className="px-5">
         <Col className="px-5 d-flex justify-content-between">
           <LogoCont className="logo-cont">
             <Link to="/">
-              <Image imgName="acumenlegal-logo.png" />
+              <Image imgName="logo.png" />
             </Link>
 
           </LogoCont>
