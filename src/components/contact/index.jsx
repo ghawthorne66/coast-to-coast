a38ed4f44b24e90d78824c2eaaccf7b45327af88
SherozeAli <sherozeali777@gmail.com>
2020-06-03 13:03:33 +0500

    contact page template done one of its section yet to design

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../common/page-header'
+import Content from './content'
+
+const Contact = () => (
+  <>
+    <PageHeader title="Contact Coast 2 Coast" />
+    <Content />
+  </>
+)
+export default Contact
