cf079e76478d9ce2a440dc0100e7b109b7a12db0
SherozeAli <sherozeali777@gmail.com>
2020-06-03 13:18:34 +0500

    icons remaining on contact page

--- src/components/contact/content.jsx
+++ src/components/contact/content.jsx
@@ -4,17 +4,44 @@ import {
 } from 'react-bootstrap'
 import styled from 'styled-components'
 import ContactForm from '../common/contact-form'
-import Image from '../common/image'
 
 const StyledContent = styled.section`
-& p{
-  font-size: 16px;
-  line-height: 27px;
-  font-weight: 500;
+& h1 {
+  font-family: "Asap";
+  color: ${({ theme }) => theme.colors.primary};
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 48px;
 
-  @media(max-width:768px){
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+}
+
+& h3 {
+  font-family: "Asap";
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 35px;
+
+  @media (max-width: 768px) {
     font-size: 16px;
   }
+}
+
+& p{
+  font-size: 14px;
+  line-height: 27px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.gray};
+
+  @media(max-width:768px){
+    font-size: 14px;
+  }
+}
+
+& a{
+  text-decoration:none;
 }
 `
 const Content = () => (
@@ -25,13 +52,31 @@ const Content = () => (
           <ContactForm />
         </Col>
         <Col className="mt-4 mt-lg-0" lg="5">
-          <Image isShadow imgName="covid-bg.jpg" />
-          <p className="mt-4">
-            Coast 2 Coast Refrigeration welcomes people of walks of life looking
-            for good, honest work. As a leader in the refrigeration repair industry,
-            we like seeing what fresh talent has to offer in this growing industry.
-            Opportunities may be few at present, you may leave your resume and other
-            contact info to get in touch when opportunities arise.
+          <h1>
+            Call Coast 2 Coast
+          </h1>
+          <a href="tel:3213513869">
+            <h3>
+              (919) 799-0276
+            </h3>
+          </a>
+          <h1>
+            Email Coast 2 Coast
+          </h1>
+          <a href="mailto:info@coast2coastrefrigeration.com">
+            <h3>
+              info@coast2coastrefrigeration.com
+            </h3>
+          </a>
+          <h1>
+            Hours
+          </h1>
+          <p>
+            Monday – Friday: 6 AM – 6 PM
+            <br />
+            Weekend: By Appointment Only
+            <br />
+            Serving all of the San Diego area
           </p>
         </Col>
       </Row>
